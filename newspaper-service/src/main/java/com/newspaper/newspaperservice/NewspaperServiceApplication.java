0bb53a353bdc1f9b6c42b253d3d8433b8d38f0d9
George Dill <gdill@nvisia.com>
2020-06-04 11:15:55 -0500

    spring cloud configuration

--- newspaper-service/src/main/java/com/newspaper/newspaperservice/NewspaperServiceApplication.java
+++ newspaper-service/src/main/java/com/newspaper/newspaperservice/NewspaperServiceApplication.java
@@ -2,8 +2,10 @@ package com.newspaper.newspaperservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class NewspaperServiceApplication {
 
 	public static void main(String[] args) {
