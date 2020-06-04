94d30521cb12836965ad73ca43a8ae52faf67c0f
George Dill <gdill@nvisia.com>
2020-06-04 10:54:52 -0500

    initial commit

---
+++ newspaper-service/src/main/java/com/newspaper/newspaperservice/NewspaperServiceApplication.java
@@ -0,0 +1,13 @@
+package com.newspaper.newspaperservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspaperServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewspaperServiceApplication.class, args);
+	}
+
+}
