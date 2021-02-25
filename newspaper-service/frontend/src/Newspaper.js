976e6ab184a8811defa23b0726fc13c121b61579
George Dill <gdill@nvisia.com>
2021-02-24 21:48:04 -0600

    adjusted width within single project

--- newspaper-service/frontend/src/Newspaper.js
+++ newspaper-service/frontend/src/Newspaper.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Newspaper extends React.Component {
 
-    
+
     constructor(props){
         super(props)
         this.latest = "http://localhost:8080/latest";
@@ -14,13 +14,13 @@ class Newspaper extends React.Component {
 
         this.randomMode = this.randomMode.bind(this);
         this.latestMode = this.latestMode.bind(this);
-        
-        this.loadText = (url) => { 
-            fetch(url, 
+
+        this.loadText = (url) => {
+            fetch(url,
                 {mode: "cors",
                  method: "GET"})
                 .then( response => response.json())
-                .then( json => this.setState({story: json}));    
+                .then( json => this.setState({story: json}));
         }
     }
 
@@ -31,14 +31,14 @@ class Newspaper extends React.Component {
     latestMode = () => {
         this.loadText(this.latest);
     }
-    async componentDidMount() { 
+    async componentDidMount() {
         this.latestMode();
     }
 
     render() {
         const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
         let date = new Date(this.state.story?.date);
-        
+
         return(
             <div className="frontPage">
                 <div className="stripe">
@@ -59,17 +59,17 @@ class Newspaper extends React.Component {
                 </div>
                 <div className="story">
                     {this.state.story?.jetpack_featured_media_url ?
-                        <div className="picture"><img src={this.state.story?.jetpack_featured_media_url}></img></div>
+                        <img class="picture" src={this.state.story?.jetpack_featured_media_url}></img>
                         : ""
                     }
                     <div className="byline">
                         <p><b>By CHRISTINA CLARK</b></p>
                         <p>christina.clark@leaderpub.com</p>
                     </div>
-                    
                     <div dangerouslySetInnerHTML={{
                             __html: this.state.story?.content?.rendered
                         }}></div>
+
                 </div>
             </div>
         )
